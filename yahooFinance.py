6aba36778f1a01e67110138c98d1dc2701a36c25
Amit Bergman <ambergma@microsoft.com>
2020-05-01 16:33:28 +0300

    changes

--- yahooFinance.py
+++ yahooFinance.py
@@ -1,43 +1,71 @@
 import yfinance as yf
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import smtplib, ssl
+from openpyxl import load_workbook
+from datetime import datetime, date
 
-msft = yf.Ticker("MSFT")
-ibm = yf.Ticker("IBM")
+def mainFunction():
+    (msft,ibm) = createTickers("MSFT", "IBM")
+    msft_price = getPrice(msft)
+    ibm_price = getPrice(ibm)
 
-msft_price = msft.info['regularMarketPreviousClose']
-ibm_price = ibm.info['regularMarketPreviousClose']
+    prices_array = [msft_price, ibm_price]
 
-prices = [msft_price, ibm_price]
-print(prices)
+    sendEmailWithPrices(prices_array)
+
+    date_today = date.today()
+    prices_array.append(date_today)
+    writeRowToExcel("stocks.xlsx", prices_array)
+
+def writeRowToExcel(excelName, rowToAdd):
+
+    wb = load_workbook(excelName)
+    # Select First Worksheet
+    ws = wb.worksheets[0]
+
+    ws.append(rowToAdd)
+
+    wb.save(excelName)
+    wb.close()
+    
+
+def createTickers(stockName1, stockName2):
+    ticker1 = yf.Ticker(stockName1)
+    ticker2 = yf.Ticker(stockName2)
+    return (ticker1, ticker2)
+
+def getPrice(stock):
+    return stock.info['regularMarketPreviousClose']
+
 
 """The first step is to create an SMTP object, each object is used for connection 
 with one server."""
 
-import smtplib
-server = smtplib.SMTP('smtp.gmail.com', 587)
+def sendEmailWithPrices(prices_array):
 
-import smtplib, ssl
-
-port = 465  # For SSL
-password = "Beni1234"
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    port = 465  # For SSL
+    password = "Beni1234"
 
 # Create a secure SSL context
-context = ssl.create_default_context()
-text = f"The prices of msft stock today was: {msft_price}.\n The price of IBM was: {ibm_price}"
+    context = ssl.create_default_context()
+    text = f"The prices of msft stock today was: {prices_array[0]}.\n The price of IBM was: {prices_array[1]}"
 
-message = MIMEMultipart("alternative")
-message["Subject"] = "Report for Amit"
-message["From"] = "123"
-message["To"] = "receiver_email"
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Report for Amit"
+    message["From"] = "Amit"
+    message["To"] = "amitbergman@gmail.com"
 
-part1 = MIMEText(text, "plain")
-message.attach(part1)
+    part1 = MIMEText(text, "plain")
+    message.attach(part1)
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("benbenelieli123beni@gmail.com", password)
-    # TODO: Send email here
-    server.sendmail("benbenelieli123beni@gmail.com", "amitbergman@gmail.com", message.as_string())
-    print(message.as_string())
-    print("cool!")
-    
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("benbenelieli123beni@gmail.com", password)
+        # TODO: Send email here
+        server.sendmail("benbenelieli123beni@gmail.com", "amitbergman@gmail.com", message.as_string())
+        print(message.as_string())
+        print("cool! Email sent!")
+
+
+mainFunction()
